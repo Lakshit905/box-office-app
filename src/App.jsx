08ab0811a48efb0f7f88882645ce536f12fe5720
Lakshit <lakshit011999@gmail.com>
2023-08-18 15:03:21 +0530

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import Starred from './components/Starred';
+import Home from './components/Home';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
 function App() {
-  return    <div> Hello </div>
+  return (
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element ={<Home/>}/>
+          <Route path="Starred" element ={<Starred/>}/>
+          <Route path="*" element={<div>Not Found</div>} />
+          {/* // <Route path="/" element={<App />}>
+          //   <Route index element={<Home />} />
+          //   <Route path="teams" element={<Teams />}>
+          //     <Route path=":teamId" element={<Team />} />
+          //     <Route path="new" element={<NewTeamForm />} />
+          //     <Route index element={<LeagueStandings />} />
+          //   </Route>
+          // </Route>
+          // <Route element={<PageLayout />}>
+          //   <Route path="/privacy" element={<Privacy />} />
+          //   <Route path="/tos" element={<Tos />} />
+          // </Route>
+          // <Route path="contact-us" element={<Contact />} /> */}
+        </Routes>
+      </BrowserRouter>
+  );
   
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import Starred from "./Starred";
+
+const Home = () => {
+    return (
+    <div>
+        <Link to="/starred">Go to Starred </Link>
+    </div>
+    
+    )
+}
+
+export default Home;
