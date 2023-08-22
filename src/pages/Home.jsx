8276de7bdb18df1c52cba4f1330c15116ca48516
Lakshit <lakshit011999@gmail.com>
2023-08-22 16:18:20 +0530

    fetch and display data using tvmaze api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,54 @@
 import { Link } from "react-router-dom";
-import Starred from "./Starred";
+import { useState } from "react";
+import { searchForShows } from "../api/tvmaze";
 
 const Home = () => {
+
+    const[searchStr,setSearchStr] = useState('');
+    const[apiData, setApiData] = useState(null);
+    const[apiDataError, setApiDataError] = useState(null);
+
+    const onSearchStrChange = (ev)=>{
+        setSearchStr(ev.target.value);
+    };
+
+    const onSearch = async (ev) =>{
+        ev.preventDefault();
+
+        try {
+            setApiDataError(null);
+            const result =  await searchForShows(searchStr);
+            setApiData(result);
+        } catch (error) {
+            setApiDataError(error);
+         }
+        
+    };
+
+    const renderApiData = () =>{
+
+        if(apiDataError){
+            return <div>Error Occured : {apiDataError.message}</div>
+
+        }
+
+        if(apiData){
+            return apiData.map((data) =>(
+                <div key={data.show.id}>{data.show.name}</div>
+            ) );
+        }
+
+        return null;
+    }
+
     return (
     <div>
-        Home
+        <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchStrChange}/>
+        <button type="submit">Search</button>
+        </form>
+        <div>{renderApiData()}</div>
+    
     </div>
     
     )
