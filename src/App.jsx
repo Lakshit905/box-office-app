2dae9a851d9a5a824f2e4b446dd0c880195f86b5
Lakshit <lakshit011999@gmail.com>
2023-09-01 01:19:24 +0530

    display actor and show data

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Starred from './pages/Starred';
 import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
+import Show from './pages/Show';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="Starred" element={<Starred />} />
         </Route>
+
+        <Route path="/show/:showId" element={<Show />} />
+
         <Route path="*" element={<div>Not Found</div>} />
         {/* // <Route path="/" element={<App />}>
           //   <Route index element={<Home />} />
